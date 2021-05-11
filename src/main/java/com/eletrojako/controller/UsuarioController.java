f9f76770161b3010766fe40f6619f9c4b6b0d5d3
João Buratto <46575243+joaoburatto@users.noreply.github.com>
2021-05-11 10:35:49 -0300

    Basic user stuff

--- src/main/java/com/eletrojako/controller/UsuarioController.java
+++ src/main/java/com/eletrojako/controller/UsuarioController.java
@@ -1,11 +1,14 @@
 package com.eletrojako.controller;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +23,10 @@ public class UsuarioController {
 	@Autowired
 	private UsuarioRepository repository;
 	
+	@GetMapping("/")
+	public List<Usuario> getUsuarios() {
+		return repository.findAll();
+	}
 	@GetMapping("/{id}")
 	public Optional<Usuario> getUsuario(@PathVariable Long id) {
 		return repository.findById(id);
@@ -30,4 +37,20 @@ public class UsuarioController {
 		return repository.save(usuario);
 	}
 	
+	@PutMapping("/atualizar/{id}")
+	public Usuario atualizarUsuario(@PathVariable int id, @RequestBody Usuario usuario) {
+		usuario.setId((long) id);
+		
+		repository.save(usuario);
+		
+		return usuario;
+	}
+	
+	@DeleteMapping("/deletar/{id}")
+	public Usuario deletarUsuario(@PathVariable int id) {
+		Usuario usuario = repository.findById((long) id).get();
+		
+		repository.delete(usuario);
+		return usuario;
+	}
 }
---
+++ src/main/java/com/eletrojako/models/Produto.java
@@ -0,0 +1,5 @@
+package com.eletrojako.models;
+
+public class Produto {
+
+}
